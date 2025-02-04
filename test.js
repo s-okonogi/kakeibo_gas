e1cecdd7ac4b4d9dc835ab3dd581b2806068bdc8
s-okonogi <okonogisyunta@gmail.com>
2025-02-04 03:45:38 +0000

    tukigbetu kouhinndo html sakusei

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
+function doGet(e){
+  const template = HtmlService.createTemplateFromFile("monthly_high");
+  const html_output = template.evaluate();
+  html_output.setTitle("月間支出高");
+  return html_output;
+}
+
+
 function myFunction() {
   const mdl = new Model();
 
